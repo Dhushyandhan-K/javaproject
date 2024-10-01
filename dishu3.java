d9277745441afff18e46989b8a2a49b63bc4e25d
Dhushyandhan-K <dishudishu706@gmail.com>
2024-10-01 15:51:25 +0530

    Add files via upload

---
+++ dishu3.java
@@ -0,0 +1,12 @@
+class conversionautomatic
+{
+	public static void main(String[] args)
+	{
+	    int i=1000;
+	    long l=i;
+	    float f=1;
+		System.out.println("int value"+i);
+		System.out.println("long value"+l);
+		System.out.println("float value"+f);
+	}
+}
